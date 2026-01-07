0d19e196b7a7432fc04429550f3c92151ecc6de4
lucie-devcode <lucie.devcode@gmail.com>
2026-01-07 15:32:44 +0100

    Configuration du middleware d'authentification

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,17 +1,18 @@
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
 
 const bookCtrl = require("../controllers/book");
 
 // Route POST pour créer un book
-router.post("/", bookCtrl.createBook);
+router.post("/", auth, bookCtrl.createBook);
 // Route PUT pour modifier un book
-router.put("/:id", bookCtrl.modifyBook);
+router.put("/:id", auth, bookCtrl.modifyBook);
 // Route DELETE pour supprimer un book
-router.delete("/:id", bookCtrl.deleteBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
 // Route GET pour récupérer un book spécifique
-router.get("/:id", bookCtrl.getOneBook);
+router.get("/:id", auth, bookCtrl.getOneBook);
 // Route GET pour récupérer tous les books
-router.get("/", bookCtrl.getAllBooks);
+router.get("/", auth, bookCtrl.getAllBooks);
 
 module.exports = router;
