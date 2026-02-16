ae7281576310caff5cd38f0642a05815473b0c99
lucie-devcode <lucie.devcode@gmail.com>
2026-02-16 09:36:38 +0100

    update

--- backend/app.js
+++ backend/app.js
@@ -37,5 +37,4 @@ app.use(express.json());
 app.use("/api/books", bookRoutes);
 app.use("/api/auth", userRoutes);
 app.use("/images", express.static(path.join(__dirname, "images")));
-
 module.exports = app;
--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,11 +1,11 @@
 const Book = require("../models/Book");
 
 exports.createBook = (req, res, next) => {
-  const bookObject = JSON.parse(req.body.thing);
+  const bookObject = JSON.parse(req.body.book);
   delete bookObject._id;
   delete bookObject._userId;
   const book = new Book({
-    ...req.body,
+    ...bookObject,
     userId: req.auth.userId,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
   });
@@ -16,24 +16,24 @@ exports.createBook = (req, res, next) => {
 };
 
 exports.modifyBook = (req, res, next) => {
-  const thingObject = req.file
+  const bookObject = req.file
     ? {
-        ...JSON.parse(req.body.thing),
+        ...JSON.parse(req.body.book),
         imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
       }
     : { ...req.body };
 
-  delete thingObject._userId;
-  Thing.findOne({ _id: req.params.id })
-    .then((thing) => {
-      if (thing.userId != req.auth.userId) {
+  delete bookObject._userId;
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (book.userId != req.auth.userId) {
         res.status(401).json({ message: "Not authorized" });
       } else {
-        Thing.updateOne(
+        Book.updateOne(
           { _id: req.params.id },
-          { ...thingObject, _id: req.params.id },
+          { ...bookObject, _id: req.params.id },
         )
-          .then(() => res.status(200).json({ message: "Objet modifié!" }))
+          .then(() => res.status(200).json({ message: "Livre modifié!" }))
           .catch((error) => res.status(401).json({ error }));
       }
     })
